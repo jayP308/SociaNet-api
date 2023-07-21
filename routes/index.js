b29046542e2d777133ecd98269b29f84430b53d4
Junnel Padilla <junnelpadilla307@gmail.com>
2023-07-20 23:24:18 -0700

    final commit

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 const apiRoutes =require ('./apiRoutes');
 
-router.request('/api', apiRoutes);
+router.use('/api', apiRoutes);
 
-module.exports = routers;
+module.exports = router;
